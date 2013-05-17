1f7e49da8146da1a4f3f2c2ecd3b29035f32c47b
Neil Mather <ngmather@gmail.com>
2013-05-17 23:26:03 +0100

    Initial commit.

---
+++ __NAME__/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace __NAME__.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
